b90041a2aa8458fd3f1e7e1c2e31da061e1fa13c
bryanpaluch <bryanpaluch@gmail.com>
2013-04-09 13:22:32 -0700

    update package.json

--- config/config.example.js
+++ config/config.example.js
@@ -1,14 +1,15 @@
 module.exports = {
 	development: {
+    httpport: 8080,
     //sip gateway that supports PeerConnection SDP 
     //Asterisks server with patches
 		outboundproxy: '10.255.132.180:5060',
     //sip domain of your sip gateway
-    domain: 'cabletownlabs.com',
+    domain: 'comcastlabs.com',
     //ip and host of the machine the this is running on
     localHost: '10.255.132.197:5060',
     //your organizations name
-    org: 'CableTownLabs'
+    org: 'ComcastLabs'
 	},
 	test: {
 
